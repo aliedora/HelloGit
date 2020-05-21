b24dd7e4972c4b533505b00cb92ab76814c9291b
z.prokhorenko <z.prokhorenko@corp.mail.ru>
2020-05-21 21:55:44 +0300

    cherry_commit

---
+++ src/HelloGit2.java
@@ -0,0 +1,12 @@
+public class HelloGit2 {
+
+    public void printSmth() {
+        System.out.println("Hello Git");
+    }
+
+    public void printSmth2() {
+        System.out.println("Hello Git");
+    }
+
+
+}
