59e9ccae427891ff6fe74ed8041208ce88a16445
z.prokhorenko <z.prokhorenko@corp.mail.ru>
2020-05-21 22:18:06 +0300

    commit

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -4,11 +4,6 @@ public class HelloGit {
         System.out.println("Hello Git");
     }
 
-    public void printSmth3() {
-        System.out.println("Hello Git");
-        System.out.println("Hello Git22222");
-    }
-
     public void printSmth2() {
 
     }
